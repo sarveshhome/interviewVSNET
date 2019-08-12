5c69777720d72f4273981308a6a76d2268e7a44c
sarveshhome <sarvesh.carlton@gmail.com>
2019-08-12 14:53:35 +0100

    refOut

---
+++ ConsoleApp1/OverloadingWithRef.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class OverloadingWithRef
+    {
+        static void Main(string[] args)
+        {
+            string Name= "Tarun";
+            Student objStudent = new Student();
+
+            objStudent.profile(ref Name, "Goa");
+            objStudent.profile(Name, "Shimla");            
+        }
+    }
+    class Student
+    {
+        static Student()
+        {
+
+        }
+        public string profile(string Name, string Address)
+        {
+            return "";
+        }
+        public string profile(ref string Name, string Address)
+        {
+            Name = "Hina";
+            return "";
+        }
+        //public string profile(out string Name, string Address)
+        //{
+        //    Name = "Ta";
+        //    return "";
+        //}
+
+    }
+}
